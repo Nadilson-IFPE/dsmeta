c8d6cc5de34358ee9f49deba088c5f4ae2115c53
Nadilson <nadilson@protonmail.com>
2022-07-15 19:07:48 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,27 @@
 import icon from "../../assets/img/notification-icon.svg";
 
 import "./styles.css";
+import { BASE_URL } from "./../../utils/request";
+import axios from "axios";
 
-function NotificationButton() {
+type Props = {
+  saleId: number;
+};
+
+function handleClick(id: number) {
+  axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
+    console.log("Sucesso! ID: " + id);
+  });
+}
+
+function NotificationButton({ saleId }: Props) {
   return (
-    <div className="dsmeta-red-btn">
+    <div
+      className="dsmeta-red-btn"
+      onClick={() => {
+        handleClick(saleId);
+      }}
+    >
       <img src={icon} alt="Notificar" />
     </div>
   );
